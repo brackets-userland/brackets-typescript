19b4b390e131359efa2f870b136621dd1a40ce09
Martin Zagora <zaggino@gmail.com>
2016-07-31 22:03:04 +1000

    bad direction, check out createLanguageService later

--- src/node/errors-for-file.js
+++ src/node/errors-for-file.js
@@ -4,10 +4,35 @@
 // from https://github.com/TypeStrong/atom-typescript/blob/master/lib/main/lang/projectService.ts#L394
 
 var fsu = require('./fsu');
+var path = require('path');
+var projectByFilePath = {};
 
 function consistentPath(query) {
   if (!query.filePath) { return; }
   query.filePath = fsu.consistentPath(query.filePath);
+}
+
+function getOrCreateProjectFile(filePath) {
+  // If we are asked to look at stuff in lib.d.ts create its own project
+  if (path.dirname(filePath) === languageServiceHost.typescriptDirectory) {
+      return tsconfig.getDefaultInMemoryProject(filePath);
+  }
+
+  var projectFile = tsconfig.getProjectSync(filePath);
+  queryParent.setConfigurationError({ projectFilePath: projectFile.projectFilePath, error: null });
+  return projectFile;
+}
+
+function getOrCreateProject(filePath) {
+  filePath = fsu.consistentPath(filePath);
+
+  if (projectByFilePath[filePath]) {
+    return projectByFilePath[filePath];
+  }
+
+  var projectFile = getOrCreateProjectFile(filePath);
+  var project = cacheAndCreateProject(projectFile);
+  return project;
 }
 
 module.exports = function errorsForFile(query) {
