0e4824a50e2333a51d38ef7e5c66c96738a0744f
Martin Zagora <zaggino@gmail.com>
2016-08-08 19:48:18 +1000

    Version 0.5.0

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 define(function (require, exports, module) {
   'use strict';
 
+  require('./node_modules/brackets-inspection-gutters/dist/main')();
   require('dist/language-definition')();
   require('dist/file-watching')();
   require('dist/code-inspection')();
--- src/code-inspection.ts
+++ src/code-inspection.ts
@@ -1,21 +1,37 @@
+import { CodeInspectionReport } from '../node_modules/brackets-inspection-gutters/src/main.d.ts';
+
 define(function (require, exports, module) {
-  'use strict';
 
   const CodeInspection = brackets.getModule('language/CodeInspection');
   const LanguageManager = brackets.getModule('language/LanguageManager');
   const ProjectManager = brackets.getModule('project/ProjectManager');
+  const PackageJson = JSON.parse(require('text!../package.json'));
+  const EXTENSION_NAME = PackageJson.name;
+  const EXTENSION_UNIQUE_NAME = 'zaggino.' + EXTENSION_NAME;
   const nodeDomain = require('./node-domain');
+  const log = require('./log');
   const LINTER_NAME = 'TypeScript';
 
   function handleScanFile(text, fullPath) {
     throw new Error(LINTER_NAME + ' sync code inspection is not available, use async for ' + fullPath);
   }
 
-  function handleScanFileAsync(text, fullPath) {
+  function handleScanFileAsync(text, fullPath): JQueryPromise<CodeInspectionReport> {
     const deferred = $.Deferred();
     const projectRoot = ProjectManager.getProjectRoot().fullPath;
     nodeDomain.exec('getDiagnostics', projectRoot, fullPath, text)
-      .then(function (report) {
+      .then(function (report: CodeInspectionReport) {
+
+        // set gutter marks using brackets-inspection-gutters module
+        const w = (<any> window);
+        if (w.bracketsInspectionGutters) {
+          w.bracketsInspectionGutters.set(
+            EXTENSION_UNIQUE_NAME, fullPath, report, true
+          );
+        } else {
+          log.error(`No bracketsInspectionGutters found on window, gutters disabled.`);
+        }
+
         deferred.resolve(report);
       }, function (err) {
         deferred.reject(err);
