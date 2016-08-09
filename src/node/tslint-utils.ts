236504ccbaac71e29dface27cb3041da9b41fb7f
Martin Zagora <zaggino@gmail.com>
2016-08-10 08:08:09 +1000

    get rid of ternary if

--- src/node/tslint-utils.ts
+++ src/node/tslint-utils.ts
@@ -2,16 +2,20 @@ import * as TSLint from 'tslint';
 import { ILinterOptionsRaw, LintResult } from 'tslint/lib/lint';
 
 export function mapLintResultFailures(failures): Array<CodeInspectionError> {
-  return failures ? failures.map(failure => {
-    return {
-      type: 'problem_type_warning',
-      message: failure.failure + ' [' + failure.ruleName + ']',
-      pos: {
-        line: failure.startPosition.lineAndCharacter.line,
-        ch: failure.startPosition.lineAndCharacter.character
-      }
-    };
-  }) : [];
+  if (failures) {
+    return failures.map(failure => {
+      return {
+        type: 'problem_type_warning',
+        message: failure.failure + ' [' + failure.ruleName + ']',
+        pos: {
+          line: failure.startPosition.lineAndCharacter.line,
+          ch: failure.startPosition.lineAndCharacter.character
+        }
+      };
+    });
+  } else {
+    return [];
+  }
 }
 
 export function executeTsLint(fullPath, code, tsLintConfig, program): Array<CodeInspectionError> {
